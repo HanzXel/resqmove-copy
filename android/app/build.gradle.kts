9f9477a3c80906a5046ef27f254cef3f631eef6d
HanzXel <hanzbunado02@gmail.com>
2026-05-16 18:27:12 +0800

    update ulit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,9 +11,10 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    isCoreLibraryDesugaringEnabled = true
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
@@ -47,4 +48,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
